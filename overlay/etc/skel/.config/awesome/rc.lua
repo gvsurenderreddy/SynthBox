3eb6420084709dff07528cc879b35c2f91489052
sean <seanbutnotheard@gmail.com>
2011-05-20 09:52:03 -0400

    first stab at using shifty for dynamic workspace tagging

--- overlay/etc/skel/.config/awesome/rc.lua
+++ overlay/etc/skel/.config/awesome/rc.lua
@@ -6,6 +6,8 @@ require("awful.rules")
 require("beautiful")
 -- Notification library
 require("naughty")
+-- Dynamic tagging and window matching
+require("shifty")
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
@@ -40,13 +42,36 @@ layouts =
 }
 -- }}}
 
--- {{{ Tags
--- Define a tag table which hold all screen tags.
-tags = {}
-for s = 1, screen.count() do
-    -- Each screen has its own tag table.
-    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[7])
-end
+-- {{{ Shifty configuration
+
+-- tag settings
+shifty.config.tags = {
+    ["synthbox"] = { position = 1, exclusive = true, spawn = "gladish", init = true },
+}
+
+
+-- client settings
+-- order here matters, early rules will be applied first
+shifty.config.apps = {
+         { match = { "ardour" } , tag = "ardour" } ,
+         { match = { "mixer" } , tag = "mixer" } ,
+         { match = { "ladiconf" }, tag = "synthbox", float = true },
+         { match = { "bristol" }, tag = "bristol", float = true },
+         { match = { "" }, buttons = {
+                             button({ }, 1, function (c) client.focus = c; c:raise() end),
+                             button({ modkey }, 1, function (c) awful.mouse.client.move() end),
+                             button({ modkey }, 3, awful.mouse.client.resize ), }, },
+}
+
+-- tag defaults
+shifty.config.defaults = {
+  layout = awful.layout.suit.tile,
+  ncol = 1,
+  mwfact = 0.60,
+  floatBars=true,
+}
+
+shifty.init()
 -- }}}
 
 -- {{{ Menu
@@ -123,6 +148,7 @@ mytasklist.buttons = awful.util.table.join(
                                               if client.focus then client.focus:raise() end
                                           end))
 
+
 for s = 1, screen.count() do
     -- Create a promptbox for each screen
     mypromptbox[s] = awful.widget.prompt({ layout = awful.widget.layout.horizontal.leftright })
@@ -160,6 +186,9 @@ for s = 1, screen.count() do
     }
 end
 -- }}}
+
+-- Point Shifty to the taglist widget
+shifty.taglist = mytaglist
 
 -- {{{ Mouse bindings
 root.buttons(awful.util.table.join(
@@ -200,6 +229,7 @@ globalkeys = awful.util.table.join(
                 client.focus:raise()
             end
         end),
+    awful.key({ modkey,           }, "t", function() shifty.add({ rel_index = 1 }) end),
 
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
@@ -256,37 +286,33 @@ for s = 1, screen.count() do
    keynumber = math.min(9, math.max(#tags[s], keynumber));
 end
 
--- Bind all key numbers to tags.
--- Be careful: we use keycodes to make it works on any keyboard layout.
--- This should map on the top row of your keyboard, usually 1 to 9.
-for i = 1, keynumber do
-    globalkeys = awful.util.table.join(globalkeys,
-        awful.key({ modkey }, "#" .. i + 9,
-                  function ()
-                        local screen = mouse.screen
-                        if tags[screen][i] then
-                            awful.tag.viewonly(tags[screen][i])
-                        end
-                  end),
-        awful.key({ modkey, "Control" }, "#" .. i + 9,
-                  function ()
-                      local screen = mouse.screen
-                      if tags[screen][i] then
-                          awful.tag.viewtoggle(tags[screen][i])
-                      end
-                  end),
-        awful.key({ modkey, "Shift" }, "#" .. i + 9,
-                  function ()
-                      if client.focus and tags[client.focus.screen][i] then
-                          awful.client.movetotag(tags[client.focus.screen][i])
-                      end
-                  end),
-        awful.key({ modkey, "Control", "Shift" }, "#" .. i + 9,
-                  function ()
-                      if client.focus and tags[client.focus.screen][i] then
-                          awful.client.toggletag(tags[client.focus.screen][i])
-                      end
-                  end))
+-- Bind all key numbers to Shifty's tags.
+for i=1, ( shifty.config.maxtags or 9 ) do
+  
+  globalkeys = awful.util.table.join(globalkeys, awful.key({ modkey }, i,
+  function ()
+    local t = awful.tag.viewonly(shifty.getpos(i))
+  end))
+  globalkeys = awful.util.table.join(globalkeys, awful.key({ modkey, "Control" }, i,
+  function ()
+    local t = shifty.getpos(i)
+    t.selected = not t.selected
+  end))
+  globalkeys = awful.util.table.join(globalkeys, awful.key({ modkey, "Control", "Shift" }, i,
+  function ()
+    if client.focus then
+      awful.client.toggletag(shifty.getpos(i))
+    end
+  end))
+  -- move clients to other tags
+  globalkeys = awful.util.table.join(globalkeys, awful.key({ modkey, "Shift" }, i,
+    function ()
+      if client.focus then
+        local t = shifty.getpos(i)
+        awful.client.movetotag(t)
+        awful.tag.viewonly(t)
+      end
+    end))
 end
 
 clientbuttons = awful.util.table.join(
@@ -296,6 +322,8 @@ clientbuttons = awful.util.table.join(
 
 -- Set keys
 root.keys(globalkeys)
+shifty.config.globalkeys = globalkeys
+shifty.config.clientkeys = clientkeys
 -- }}}
 
 -- {{{ Rules
@@ -336,7 +364,7 @@ client.add_signal("manage", function (c, startup)
     if not startup then
         -- Set the windows at the slave,
         -- i.e. put it at the end of others instead of setting it master.
-        -- awful.client.setslave(c)
+        awful.client.setslave(c)
 
         -- Put windows in a smart way, only if they does not set an initial position.
         if not c.size_hints.user_position and not c.size_hints.program_position then
