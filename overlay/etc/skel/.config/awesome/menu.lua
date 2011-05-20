64e601c483bf585fbd16aa5b1ca956522d5ed0da
sean <seanbutnotheard@gmail.com>
2011-05-20 12:40:39 -0400

    added new programs to the main menu

--- overlay/etc/skel/.config/awesome/menu.lua
+++ overlay/etc/skel/.config/awesome/menu.lua
@@ -9,38 +9,47 @@ editor_cmd = terminal .. " -e " .. editor
 
 -- Create a laucher widget and a main menu
 
-myaudiomenu = {
-	{ "ardour", "ardour2" },
-	{ "jconvolver", "Jc_Gui" },
-	{ "hydrogen", "hydrogen" },
-	{ "qsampler", "qsampler" },
-	{ "vmpk", "vmpk" },
-	{ "jamin", "jamin" },
-	{ "--" },
+myutilsmenu = {
+	{ "terminal", terminal},
+	{ "file manager", "thunar"},
 	{ "gladish", "gladish" },
 	{ "alsamixer", terminal .. " -e alsamixer" },
 }
 
 mysystemmenu = {
-	{ "gtk2 theme switcher", "gtk-theme-switch2" },
+	{ "gtk2 theme", "gtk-theme-switch2" },
+	{ "htop", terminal .. " -e htop" },
 	{ "--" },
-	{ "awesome manual", terminal .. " -e man awesome" },
-	{ "edit awesome config", editor_cmd .. " " .. awful.util.getdir("config") .. "/rc.lua" },
-	{ "edit awesome menu", editor_cmd .. " " .. awful.util.getdir("config") .. "/menu.lua" },
-	{ "--" },
-	{ "restart awesome", awesome.restart },
-	{ "quit awesome (logoff)", awesome.quit },
+	{ "restart wm", awesome.restart },
+	{ "quit to term", awesome.quit },
 	{ "shutdown", terminal .. " -e sudo poweroff" },
 	{ "reboot", terminal .. " -e sudo reboot" },
 }
 
 mymainmenu = awful.menu.new(
 	{ items = { 
-		{ "-+=Menu=+-", ""},
-		{ "terminal", terminal},
-		{ "file manager", "thunar"},
-		{ "browser", "firefox"},
-		{ "audio", myaudiomenu},
-		{ "system", mysystemmenu},
+	{ "-+=Menu=+-", ""},
+	{ "ardour", "ardour2" },
+	{ "ams", "ams" },
+	{ "non midi sequencer", "non-sequencer" },
+	{ "seq24 midi sequencer", "seq24" },
+	{ "bristol mini", "startBristol -mini" },
+	{ "yoshimi synth", "yoshimi" },
+	{ "lv2 synth host", "zynjacku" },
+	{ "lv2 fx host", "lv2rack" },
+	{ "calf synth/fx host", "calfjackhost" },
+	{ "jconvolver", "Jc_Gui" },
+	{ "hydrogen", "hydrogen" },
+	{ "zita autotuner", "zita-at1" },
+	{ "zita reverb", "zita-rev1" },
+	{ "traditional vu meter", "jmeters -t vu system:playback_1 system:playback_2" },
+	{ "graphical volume meter", "jkmeter" },
+	{ "audio analyzer", "jaaa"},
+	--{ "linuxsampler", "qsampler" },
+	{ "vmpk", "vmpk" },
+	{ "jamin", "jamin" },
+	{ "--" },
+	{ "utilities", myutilsmenu},
+	{ "system", mysystemmenu},
 	}
 })
